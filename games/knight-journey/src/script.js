7545dd03afc8a05f58d14f3e556dce08ab074486
David D <djerfidavid661@gmail.com>
2026-04-20 22:10:31 +0200

    feat(knight-journey): implement board editing

--- games/knight-journey/src/script.js
+++ games/knight-journey/src/script.js
@@ -13,6 +13,29 @@ const editorFENTextarea = document.getElementById('editor-fen-textarea');
 const gameCurrentMovesSpan = document.getElementById('game-current-moves-span');
 const gameOptimalMovesSpan = document.getElementById('game-optimal-moves-span');
 
+const whiteKnightElement = document.getElementById('white-knight');
+const whitePawnElement = document.getElementById('white-pawn');
+const blackKingElement = document.getElementById('black-king');
+const blackQueenElement = document.getElementById('black-queen');
+const blackRookElement = document.getElementById('black-rook');
+const blackBishopElement = document.getElementById('black-bishop');
+const blackKnightElement = document.getElementById('black-knight');
+const blackPawnElement = document.getElementById('black-pawn');
+
+const pieceElements = [
+    whiteKnightElement, 
+    whitePawnElement, 
+    blackKingElement, 
+    blackQueenElement, 
+    blackRookElement, 
+    blackBishopElement, 
+    blackKnightElement, 
+    blackPawnElement
+];
+
+let editorCurrentPiece = null;
+let editorHasWhiteKnight = false;
+let editorHasBlackKing = false;
 
 
 function showPanel(panelDiv) {
@@ -25,6 +48,15 @@ function showPanel(panelDiv) {
 
 function getFEN() {
     return '8/8/8/8/8/8/8/8'; 
+}
+
+
+function clearSquare(square) {
+    Array.from(square.children).forEach(child => {
+        square.removeChild(child);
+        if (child.getAttribute('class') === 'white-knight') editorHasWhiteKnight = false;
+        if (child.getAttribute('class') === 'black-king') editorHasBlackKing = false;
+    });
 }
 
 
@@ -53,11 +85,57 @@ for (let row = 0; row < 8; row++) {
       square.classList.add('dark');
     }
 
+    square.addEventListener('click', () => {
+        if (editorCurrentPiece) {
+            const clone = editorCurrentPiece.cloneNode(true);
+            if (clone.id === 'black-king' && editorHasBlackKing) return;
+            if (clone.id === 'white-knight' && editorHasWhiteKnight) return;
+
+            clearSquare(square);
+    
+            clone.setAttribute('class', clone.id);
+
+            if (clone.id === 'white-knight') editorHasWhiteKnight = true;
+            if (clone.id === 'black-king') editorHasBlackKing = true;
+
+            clone.removeAttribute('id'); 
+            square.appendChild(clone);
+        }
+        else {
+            clearSquare(square);
+        }
+    });
+
     board.appendChild(square);
   }
 }
 
+pieceElements.forEach(pieceElement => {
+    pieceElement.addEventListener('click', () => {
+        if (pieceElement === editorCurrentPiece) {
+            editorCurrentPiece.classList.remove('editor-selected-piece');
+            editorCurrentPiece = null;
+        }
+        else {
+            editorCurrentPiece = pieceElement;
+            pieceElements.forEach(piece => {
+                piece.classList.remove('editor-selected-piece');
+            });
+            pieceElement.classList.add('editor-selected-piece');
+            console.log(editorCurrentPiece.id);
+        }
+        
 
+    });
+});
+
+document.getElementById('editor-clear-board-btn').onclick = () => {
+    editorHasBlackKing = false;
+    editorHasWhiteKnight = false;
+    Array.from(board.children).forEach(square => {
+        clearSquare(square);
+    })
+}
 
 
 showPanel(editorDiv);
